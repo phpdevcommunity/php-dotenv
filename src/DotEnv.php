9a5eb1711642732098b6ebd065d5f19a754c67f1
Jaume <jaumarar@gmail.com>
2021-02-07 11:24:20 +0100

    Added process of booleans

--- src/DotEnv.php
+++ src/DotEnv.php
@@ -5,25 +5,63 @@ namespace DevCoder;
 class DotEnv
 {
     /**
+     * Convert true and false to booleans, instead of:
+     *
+     * VARIABLE=false -> ['VARIABLE' => 'false']
+     *
+     * it will be
+     *
+     * VARIABLE=false -> ['VARIABLE' => false]
+     *
+     * default = true
+     */
+    const PROCESS_BOOLEANS = 'PROCESS_BOOLEANS';
+
+    /**
      * The directory where the .env file can be located.
      *
      * @var string
      */
     protected $path;
 
-    public function __construct(string $path)
+    /**
+     * Configure the options on which the parsed will act
+     *
+     * @var array
+     */
+    protected $options = [];
+
+    public function __construct(string $path, array $options = [])
     {
-        if(!file_exists($path)) {
+        if (!file_exists($path)) {
             throw new \InvalidArgumentException(sprintf('%s does not exist', $path));
         }
+
         $this->path = $path;
+
+        $this->processOptions($options);
     }
 
-    public function load() :void
+    private function processOptions(array $options)
+    {
+        $this->options = array_merge([
+            static::PROCESS_BOOLEANS => true
+        ], $options);
+    }
+
+    /**
+     * Processes the $path of the instances and parses the values into $_SERVER and $_ENV, also returns all the data that has been read.
+     * Skips empty and commented lines.
+     *
+     * @return array The values that have been loaded into ENV
+     */
+    public function load() :array
     {
         if (!is_readable($this->path)) {
             throw new \RuntimeException(sprintf('%s file is not readable', $this->path));
         }
+
+        $loaded = [];
 
         $lines = file($this->path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
         foreach ($lines as $line) {
@@ -34,13 +72,32 @@ class DotEnv
 
             list($name, $value) = explode('=', $line, 2);
             $name = trim($name);
-            $value = trim($value);
+            $value = $this->processValue($value);
 
             if (!array_key_exists($name, $_SERVER) && !array_key_exists($name, $_ENV)) {
                 putenv(sprintf('%s=%s', $name, $value));
                 $_ENV[$name] = $value;
                 $_SERVER[$name] = $value;
+                $loaded[$name] = $value;
             }
         }
+
+        return $loaded;
+    }
+
+    private function processValue($value) {
+        $trimmedValue = trim($value);
+
+        if (!empty($this->options[static::PROCESS_BOOLEANS])) {
+            $loweredValue = strtolower($trimmedValue);
+
+            $isBoolean = in_array($loweredValue, ['true', 'false'], true);
+
+            if ($isBoolean) {
+                return $loweredValue === 'true';
+            }
+        }
+
+        return $trimmedValue;
     }
 }
